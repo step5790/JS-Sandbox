d754f9bb94f5cf85b7f4cafd662c5d26f7353c0c
stephen <step5790@stud.kea.dk>
2023-06-27 21:51:25 +0200

    JS-Sandbox

---
+++ leap-year.js
@@ -0,0 +1,13 @@
+function isLeap(year) {
+  if (year % 4 === 0) {
+    console.log("Leap year.");
+  } else if (year % 4 !== 0) {
+    console.log("Not leap year.");
+  } else if (year % 4 === 0 && year % 100 === 0) {
+    console.log("Not leap year.");
+  } else if (year % 4 === 0 && year % 400 === 0) {
+    console.log("Leap year.");
+  }
+}
+
+isLeap(1998);
